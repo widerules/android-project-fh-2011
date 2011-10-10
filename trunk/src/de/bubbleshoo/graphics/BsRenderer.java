104b1bac0743ba0007e879af6ab2596802d00091
oliverlaudi27@googlemail.com <oliverlaudi27@googlemail.com@f1ad4089-ea60-6fa0-337a-b9bb6751d20e>
2011-10-10 15:04:10 +0000

    BsElement.java hinzugefügt + Dreieck anzeigen

--- trunk/src/de/bubbleshoo/graphics/BsRenderer.java
+++ trunk/src/de/bubbleshoo/graphics/BsRenderer.java
@@ -3,8 +3,12 @@
  */
 package de.bubbleshoo.graphics;
 
+import java.util.List;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
+import de.bubbleshoo.data.BsElement;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
@@ -16,12 +20,56 @@ import android.opengl.GLSurfaceView;
 public class BsRenderer implements GLSurfaceView.Renderer{
 
 	/**
+	 * Programm for Vertex- and Fragmentshader
+	 */
+	private int mProgram;
+	
+	/**
+	 * Position for Elements manipulated by shader
+	 */
+    private int maPositionHandle;
+    
+	/**
+	 * Elements to draw
+	 */
+	private List<BsElement>	m_lstElements;
+	
+	/**
+	 *  Vertex shader
+	 */
+	private final String 	m_vertexShaderCode = 
+        "attribute vec4 vPosition; \n" +
+        "void main(){              \n" +
+        " gl_Position = vPosition; \n" +
+        "}                         \n";
+    
+	/**
+	 * Fragment Shader
+	 */
+    private final String 	m_fragmentShaderCode = 
+        "precision mediump float;  \n" +
+        "void main(){              \n" +
+        " gl_FragColor = vec4 (0.63671875, 0.76953125, 0.22265625, 1.0); \n" +
+        "}                         \n";
+	
+	/**
 	 * Is called for each redraw of the View
 	 */
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		// Redraw background Color
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+		
+        for (BsElement bsEmt : this.m_lstElements) {
+        	// Add program to OpenGL environment
+            GLES20.glUseProgram(this.mProgram);
+        	// Prepare the triangle data
+            GLES20.glVertexAttribPointer(this.maPositionHandle, 3, GLES20.GL_FLOAT, false, 12, bsEmt.getVertexBuffer());
+            GLES20.glEnableVertexAttribArray(this.maPositionHandle);
+            
+            // Draw the triangle
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+		}
 	}
 
 	/**
@@ -38,8 +86,52 @@ public class BsRenderer implements GLSurfaceView.Renderer{
 	 */
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, this.m_vertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, this.m_fragmentShaderCode);
+
 		// Set the background frame color
 		GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		
+		this.mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
+        GLES20.glAttachShader(this.mProgram, vertexShader);   // add the vertex shader to program
+        GLES20.glAttachShader(this.mProgram, fragmentShader); // add the fragment shader to program
+        GLES20.glLinkProgram(this.mProgram);                  // creates OpenGL program executables
+        
+        // get handle to the vertex shader's vPosition member
+        this.maPositionHandle = GLES20.glGetAttribLocation(this.mProgram, "vPosition");
+	}
+	
+	/**
+	 * loads shader from its code and type
+	 * @param type
+	 * @param shaderCode
+	 * @return
+	 */
+	private int loadShader(int type, String shaderCode){
+	    
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type); 
+        
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        
+        return shader;
+    }
+	
+	/**
+	 * @param m_lstElements the m_lstElements to set
+	 */
+	public void setLstElements(List<BsElement> lstElements) {
+		this.m_lstElements = lstElements;
+	}
+
+	/**
+	 * @return the m_lstElements
+	 */
+	public List<BsElement> getLstElements() {
+		return m_lstElements;
 	}
 
 }
--- trunk/src/de/bubbleshoo/graphics/BsSurfaceView.java
+++ trunk/src/de/bubbleshoo/graphics/BsSurfaceView.java
@@ -3,6 +3,10 @@
  */
 package de.bubbleshoo.graphics;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.bubbleshoo.data.BsElement;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
@@ -13,6 +17,16 @@ import android.opengl.GLSurfaceView;
 public class BsSurfaceView extends GLSurfaceView {
 
 	/**
+	 * Renderer
+	 */
+	private BsRenderer		m_bsRenderer;
+	
+	/**
+	 * Elements to draw
+	 */
+	private List<BsElement>	m_lstElements;
+	
+	/**
 	 * Constructor
 	 * @param context
 	 */
@@ -20,8 +34,29 @@ public class BsSurfaceView extends GLSurfaceView {
 		super(context);
 		// Create an OpenGL ES 2.0 Context
 		setEGLContextClientVersion(2);
+		// Init Elements
+		initElementList();
 		// Set the renderer for drawing on the GLSurfaceView
-		setRenderer(new BsRenderer());
+		this.m_bsRenderer = new BsRenderer();
+		this.m_bsRenderer.setLstElements(this.m_lstElements);
+		setRenderer(this.m_bsRenderer);
 	}
 
+	/**
+	 * Initialize all elements to draw
+	 */
+	private void initElementList() {
+		this.m_lstElements = new ArrayList<BsElement>();
+		
+		// Dreieck
+		 float triangleCoords[] = {
+		            // X, Y, Z
+		            -0.5f, -0.25f, 0,
+		             0.5f, -0.25f, 0,
+		             0.0f,  0.559016994f, 0
+		        };
+		 
+		 BsElement emt1 = new BsElement(triangleCoords);
+		 this.m_lstElements.add(emt1);
+	}
 }
