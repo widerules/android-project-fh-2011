a806db44074bd24454cc66ee2903b9ee4470e03d
rushhunter1986 <rushhunter1986@gmail.com>
2011-11-09 13:45:45 +0000

    Map Elemente ( MAuer Busch ... ) hinzugefügt...

--- src/de/bubbleshoo/map/Map.java
+++ src/de/bubbleshoo/map/Map.java
@@ -1,5 +1,24 @@
 package de.bubbleshoo.map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map {
 
+	/*
+	 * Attribute
+	 */
+	private List<Feld> gamemap = new ArrayList<Feld>();
+	
+	
+	/*
+	 * Konstruktoren
+	 */
+	
+	
+	/*
+	 * Getter /Setter
+	 */
+	
+	
 }
--- src/de/bubbleshoo/units/Vip.java
+++ src/de/bubbleshoo/units/Vip.java
@@ -1,5 +1,13 @@
 package de.bubbleshoo.units;
 
-public class Vip {
+import android.content.Context;
+import de.bubbleshoo.data.Bs3DObject;
+
+public class Vip extends Unit{
+
+	public Vip(int[] texFile, int meshID, boolean hasTexture, Context context) {
+		super(texFile, meshID, hasTexture, context);
+		// TODO Auto-generated constructor stub
+	}
 
 }
