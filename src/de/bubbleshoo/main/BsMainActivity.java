b3b81df5f748c48938dde857224311fe0c0a3f9d
rushhunter1986 <rushhunter1986@gmail.com>
2011-11-09 10:33:54 +0000

    Sensorendaten können unter BsDataholder ausgelesen werden (Statische Klasse). Settings unter GeneralSettings. Momentan ist dort nur die Logger kategorie

--- src/de/bubbleshoo/main/BsMainActivity.java
+++ src/de/bubbleshoo/main/BsMainActivity.java
@@ -4,20 +4,49 @@
 package de.bubbleshoo.main;
 
 import de.bubbleshoo.graphics.BsSurfaceView;
+import de.bubbleshoo.sensors.BsDataholder;
+import de.bubbleshoo.settings.GeneralSettings;
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
 
 /**
  * @author oliverl
  *
  */
-public class BsMainActivity extends Activity {
+public class BsMainActivity extends Activity implements SensorEventListener{
 	/**
 	 * View to show OpenGL-Contents
 	 */
 	private GLSurfaceView		m_GLView;
 	
+	/**
+	 * Sensoren
+	 */
+		private SensorManager 	mSensorManager;
+	    private Sensor 			mAccelerometer;
+	    private Sensor 			mkompass;
+	    private Sensor 			mlight;
+	    //Attribute für Sensoren:
+	    // touch events
+	    private final int 		NONE = 0;
+	    private final int 		DRAG = 1;
+	    private final int 		ZOOM = 2;
+	    // pinch to zoom
+	    float 					oldDist = 100.0f;
+	    float 					newDist;
+	    int 					mode = 0;
+	    private final float 	TOUCH_SCALE_FACTOR = 180.0f / 320;
+	    private boolean 		kompassOn=false;
+	    private boolean 		lichtOn=false;
+    
     /** 
      * Construktor
      * Called when the activity is first created. 
@@ -26,6 +55,26 @@ public class BsMainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        //Sensoren laden
+		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		boolean accelerometerAvailable = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() > 0;
+	
+		mAccelerometer = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+		if(!mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME ) )
+		   accelerometerAvailable = false;
+		
+		//Kombass
+		if(kompassOn){
+		mkompass=mSensorManager.getSensorList(Sensor.TYPE_ORIENTATION).get(0);;
+		mSensorManager.registerListener(mCompassListener, mkompass,SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		
+		//Licht
+		if(lichtOn){
+		mlight=mSensorManager.getSensorList(Sensor.TYPE_LIGHT).get(0);;
+		mSensorManager.registerListener(mlichtListener, mlight,SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		
         this.m_GLView = new BsSurfaceView(this);
         setContentView(this.m_GLView);
     }
@@ -38,6 +87,12 @@ public class BsMainActivity extends Activity {
 		super.onPause();
 		// set the renderingthread in pausemode
 		this.m_GLView.onPause();
+		//Sensoren unregistrieren
+		mSensorManager.unregisterListener(this);
+		if(kompassOn)
+        mSensorManager.unregisterListener(mCompassListener);
+        if(lichtOn)
+        mSensorManager.unregisterListener(mlichtListener);
 	}
 
 	/** 
@@ -48,5 +103,134 @@ public class BsMainActivity extends Activity {
 		super.onResume();
 		// resume the renderingthread
 		this.m_GLView.onResume();
+		//Sensoren Registrieren
+		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		if(kompassOn)
+		mSensorManager.registerListener(mCompassListener, mkompass,SensorManager.SENSOR_DELAY_NORMAL);
+		if(lichtOn)
+		mSensorManager.registerListener(mlichtListener,mlight, SensorManager.SENSOR_DELAY_NORMAL);
 	}
+	
+	
+	/*
+	 * Events
+	 */
+	 
+	/** Wird aufgerufen wenn einer aufs Display drückt
+	 * 
+	 */
+	    @Override public boolean onTouchEvent(MotionEvent e) {
+	            float x = e.getX();
+	            float y = e.getY();
+                     
+	            switch (e.getAction()) {
+	                    case MotionEvent.ACTION_DOWN:                   // one touch: drag
+	                  Log.d(GeneralSettings.LoggerKategorie, "mode=DRAG" );
+	                  mode = DRAG;
+	                  break;
+	                    case MotionEvent.ACTION_POINTER_DOWN:   // two touches: zoom
+	                            Log.d(GeneralSettings.LoggerKategorie, "mode=ZOOM" );
+	                            oldDist = spacing(e);
+	                            
+	                            if (oldDist > 10.0f) {
+	                                    mode = ZOOM; // zoom
+	                            }
+	                            break;
+	                    case MotionEvent.ACTION_UP:             // no mode
+	                            mode = NONE;
+	                            Log.d(GeneralSettings.LoggerKategorie, "mode=NONE" );
+	                            oldDist = 100.0f;
+	                            break;
+	                    case MotionEvent.ACTION_POINTER_UP:             // no mode
+	                            mode = NONE;
+	                            Log.d(GeneralSettings.LoggerKategorie, "mode=NONE" );
+	                            oldDist = 100.0f;
+	                            break;
+	                    case MotionEvent.ACTION_MOVE:                                           // rotation
+	                            if (e.getPointerCount() > 1 && mode == ZOOM) {
+	                                    newDist = spacing(e);
+	                                    Log.d(GeneralSettings.LoggerKategorie, "OldDist: " + oldDist + ", NewDist: " + newDist);
+	                                    BsDataholder.setAbstandZwischenZweiFingern(newDist);
+	                                    if (newDist > 10.0f) {
+	                                            float scale = newDist/oldDist; // scale
+	                                            // scale in the renderer
+	                                           // renderer.changeScale(scale);
+	                                            
+	                                            oldDist = newDist;
+	                                    }
+	                                    /*if (newDist > oldDist) {              // distance increasing - scale
+	                                            renderer.increaseScale();
+	                                    }
+	                                    else if (newDist < oldDist){    // distance decreasing
+	                                            renderer.decreaseScale();
+	                                    }*/
+	                            }
+	                            else if (mode == DRAG){
+	                	            BsDataholder.setTouchModus(mode);
+	                	            BsDataholder.setTouchX(x* TOUCH_SCALE_FACTOR);
+	                	            BsDataholder.setTouchY(y* TOUCH_SCALE_FACTOR);
+	                            }
+	                            break;
+	            }
+
+	           // mAngle = y;
+	            return true;
+	    }
+	    
+	  /** Macht nichts momentan...
+	   * (non-Javadoc)
+	   * @see android.hardware.SensorEventListener#onAccuracyChanged(android.hardware.Sensor, int)
+	   */
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	    }
+
+	    /** Der Handypositionssensor / Neigungssensor
+	     * 
+	     */
+	    public void onSensorChanged(SensorEvent event) {
+	        BsDataholder.setHandykipplageX(event.values[0]* TOUCH_SCALE_FACTOR);
+            BsDataholder.setHandykipplageY(event.values[1]* TOUCH_SCALE_FACTOR);
+	    }
+	    
+	    
+	    //Kompass
+	    //Liefer 0-360 ich vermute 0 bedeutet Norden ist vom Handy oben
+	    private final SensorEventListener mCompassListener = new SensorEventListener() {
+	    	   	 
+	    	  public void onSensorChanged(SensorEvent event) {
+		    		  if(kompassOn)
+		    		  	{
+		    			  	float[] mValues = event.values;
+		    		  		BsDataholder.setKompassrichtung(event.values[0]* TOUCH_SCALE_FACTOR);
+		    		  	}
+	    		  	}
+	    	  
+	    	  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    	  }
+	    };
+	    
+	    //Licht
+	    //Wert zwischen 10 und 360... Normales licht ist ca 140. Wenn man Telefon an Ohr hält 10. 
+	    private final SensorEventListener mlichtListener = new SensorEventListener() {
+		   	 
+	  	  public void onSensorChanged(SensorEvent event) {
+		  		if(lichtOn)
+			  	{
+		  		  	float[] mValues = event.values;
+		  		  	BsDataholder.setRaumhelligkeit(event.values[0]* TOUCH_SCALE_FACTOR) ;
+			  	}
+		  	}
+	  	  
+	  	  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	  	  }
+	  };
+	    // finds spacing
+	    private float spacing(MotionEvent event) {
+	            float x = event.getX(0) - event.getX(1);
+	            float y = event.getY(0) - event.getY(1);
+	            return FloatMath.sqrt(x * x + y * y);
+	    }
+	
+	
 }
---
+++ src/de/bubbleshoo/settings/GeneralSettings.java
@@ -0,0 +1,15 @@
+package de.bubbleshoo.settings;
+
+public class GeneralSettings {
+
+/*
+ * Debugging
+ */
+	//Die Logger Kategorie
+	public static String LoggerKategorie="Bubbleshoo";
+	
+/*
+ * OpenGl	
+ */
+	
+}
