4cd1c086414613627e7c6a79f9340419ad0e7929
rushhunter1986@gmail.com <rushhunter1986@gmail.com@f1ad4089-ea60-6fa0-337a-b9bb6751d20e>
2011-11-09 13:45:45 +0000

    Map Elemente ( MAuer Busch ... ) hinzugefügt...

--- trunk/src/de/bubbleshoo/units/Unit.java
+++ trunk/src/de/bubbleshoo/units/Unit.java
@@ -3,9 +3,9 @@ package de.bubbleshoo.units;
 import android.content.Context;
 import de.bubbleshoo.data.Bs3DObject;
 
-public class Enemy extends Unit{
+public class Unit extends Bs3DObject {
 
-	public Enemy(int[] texFile, int meshID, boolean hasTexture, Context context) {
+	public Unit(int[] texFile, int meshID, boolean hasTexture, Context context) {
 		super(texFile, meshID, hasTexture, context);
 		// TODO Auto-generated constructor stub
 	}
