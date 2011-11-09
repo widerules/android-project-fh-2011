24486cdeb2f81e870ccfebf0d3b841622682ac42
rushhunter1986 <rushhunter1986@gmail.com>
2011-11-09 13:30:40 +0000

    Allgemeine Strukturen: Map, Units Enemys, Bälle

---
+++ src/de/bubbleshoo/map/Feld.java
@@ -0,0 +1,33 @@
+package de.bubbleshoo.map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.bubbleshoo.units.Enemy;
+
+public class Feld {
+
+	/*
+	 * Attribute
+	 */
+	private int 	feldhoehe=0;
+		
+	private boolean isStartFeld=false;
+	private boolean isEndFeld=false;
+	private boolean isEnemyStartFeld=false;
+	
+	
+	private List<Enemy> enemys=new ArrayList<Enemy>();
+	private List<Enemy> vips=new ArrayList<Enemy>();
+
+	/*
+	 * Konstruktoren
+	 */
+	
+	
+	
+	/*
+	 * Getter Setter
+	 */
+	
+}
