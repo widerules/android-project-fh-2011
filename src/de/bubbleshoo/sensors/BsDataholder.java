fecee6a7dc265c769e5cbdedc498c63325a19694
oliverlaudi27 <oliverlaudi27@gmail.com>
2011-11-25 11:34:03 +0000

    Sensor wird nun interpoliert über die Zeit dargestellt

--- src/de/bubbleshoo/sensors/BsDataholder.java
+++ src/de/bubbleshoo/sensors/BsDataholder.java
@@ -9,6 +9,7 @@ public class BsDataholder {
     private final int DRAG = 0;
     private final int ZOOM = 0;
     
+    
     /*
      * Attribute
      */
@@ -19,6 +20,9 @@ public class BsDataholder {
 	private static int 		touchModus = 0;
 
 	//Accilerator
+	private static float[] 	m_kippX = new float[4];
+    private static float[] 	m_kippY = new float[4];
+    private static float[] 	m_kippZ = new float[4];
 	private static float 	handykipplageX = 0;
 	private static float 	handykipplageY = 0;
 	private static float 	handykipplageZ = 0;
@@ -76,22 +80,43 @@ public class BsDataholder {
 		BsDataholder.raumhelligkeit = raumhelligkeit;
 	}
 	public static float getHandykipplageX() {
-		return handykipplageX;
+		if (m_kippX[3] == 0.0)
+			return m_kippX[0];
+		else
+			return ((0.25f * m_kippX[0]) + (0.25f * m_kippX[1]) + (0.25f * m_kippX[2]) + (0.25f * m_kippX[3]));
 	}
 	public static void setHandykipplageX(float handykipplageX) {
-		BsDataholder.handykipplageX = handykipplageX;
+		m_kippX[3] = m_kippX[2];
+		m_kippX[2] = m_kippX[1];
+		m_kippX[1] = m_kippX[0];
+		m_kippX[0] = handykipplageX;
+		//BsDataholder.handykipplageX = handykipplageX;
 	}
 	public static float getHandykipplageY() {
-		return handykipplageY;
+		if (m_kippY[3] == 0.0)
+			return m_kippY[0];
+		else
+			return ((0.25f * m_kippY[0]) + (0.25f * m_kippY[1]) + (0.25f * m_kippY[2]) + (0.25f * m_kippY[3]));
 	}
 	public static void setHandykipplageY(float handykipplageY) {
-		BsDataholder.handykipplageY = handykipplageY;
+		m_kippY[3] = m_kippY[2];
+		m_kippY[2] = m_kippY[1];
+		m_kippY[1] = m_kippY[0];
+		m_kippY[0] = handykipplageY;
+		//BsDataholder.handykipplageY = handykipplageY;
 	}
 	public static float getHandykipplageZ() {
-		return handykipplageZ;
+		if (m_kippZ[3] == 0.0)
+			return m_kippZ[0];
+		else
+			return ((0.25f * m_kippZ[0]) + (0.25f * m_kippZ[1]) + (0.25f * m_kippZ[2]) + (0.25f * m_kippZ[3]));
 	}
 	public static void setHandykipplageZ(float handykipplageZ) {
-		BsDataholder.handykipplageZ = handykipplageZ;
+		m_kippZ[3] = m_kippZ[2];
+		m_kippZ[2] = m_kippZ[1];
+		m_kippZ[1] = m_kippZ[0];
+		m_kippZ[0] = handykipplageZ;
+		//BsDataholder.handykipplageZ = handykipplageZ;
 	}
 	public static void setTouchModus(int touchModus) {
 		BsDataholder.touchModus = touchModus;
