6ebbfd8e37c777e2aa5eabbb55f9e793beff14e5
rushhunter1986@gmail.com <rushhunter1986@gmail.com@f1ad4089-ea60-6fa0-337a-b9bb6751d20e>
2012-01-18 19:19:40 +0000

    Menü eingebaut

---
+++ trunk/src/de/bubbleshoo/main/BsMainMenu.java
@@ -0,0 +1,73 @@
+package de.bubbleshoo.main;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class BsMainMenu extends Activity {
+	Handler handler;
+	ImageButton play, credits;
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Timer t = new Timer();
+        TimerTask runTask;
+        handler = new Handler();
+        runTask = new TimerTask() {
+            public void run() {
+                    handler.post(new Runnable() {
+                            public void run() {
+                            	setContentView(R.layout.home);
+                            	init();
+                            }
+                   });
+            }};
+
+
+        t.schedule(runTask, 3000);
+        
+        
+    }
+	
+	void init()
+	{
+		play = (ImageButton)findViewById(R.id.button1);
+		credits = (ImageButton)findViewById(R.id.button2);
+		
+		play.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent;  // Reusable Intent for each tab
+			    // Create an Intent to launch an Activity for the tab (to be reused)
+			    intent = new Intent().setClass(BsMainMenu.this, BsMainActivity.class);
+			    startActivityForResult(intent, 0);
+			}
+		});
+		
+		credits.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(BsMainMenu.this);
+				builder.setMessage("Oliver Laudi\nJorn Bettermann\nEva Bojorges")
+				       .setCancelable(true)
+				       .setTitle("Credits")
+				       .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				           }
+				       });
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+		});
+	}
+
+}
